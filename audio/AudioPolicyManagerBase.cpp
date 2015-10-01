618cac493a6f6668e25a6569b286fc32f2e0720f
hmz007 <hmz007@gmail.com>
2015-10-01 14:29:23 +0800

    Import source for Samsung S5P4418
    Ref: 4418-kitkat-dev-20150624.tar.bz2 (681f0ca)

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -3004,7 +3004,7 @@ float AudioPolicyManagerBase::computeVolume(int stream,
     // if volume is not 0 (not muted), force media volume to max on digital output
     if (stream == AudioSystem::MUSIC &&
         index != mStreams[stream].mIndexMin &&
-        (device == AUDIO_DEVICE_OUT_AUX_DIGITAL ||
+        (//device == AUDIO_DEVICE_OUT_AUX_DIGITAL ||
          device == AUDIO_DEVICE_OUT_DGTL_DOCK_HEADSET ||
          device == AUDIO_DEVICE_OUT_USB_ACCESSORY ||
          device == AUDIO_DEVICE_OUT_USB_DEVICE)) {
